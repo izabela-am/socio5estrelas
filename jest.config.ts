0769cccdf8b45df0233a1445464713469aad3a66
Izabela <izabela.m.alfenas@gmail.com>
2022-02-22 07:37:39 -0300

    project config

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  displayName: 'root-tests',
+  testMatch: ['<rootDir>/**/*.test.ts'],
+  testEnvironment: 'node',
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/services/**/*.ts',
+    '<rootDir>/src/utils/**/*.ts',
+    '!<rootDir>/src/utils/module-alias.ts',
+    '!<rootDir>/src/utils/validator.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text-summary', 'lcov'],
+  preset: 'ts-jest',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+};
---
+++ src/utils/credentials.ts
@@ -0,0 +1,6 @@
+import API_SECRETS from '../../secrets/api.json';
+
+export const API = {
+  baseURL: API_SECRETS.baseURL,
+  generalData: API_SECRETS.generalData
+};
