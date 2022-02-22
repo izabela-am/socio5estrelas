fbdca9016cf739cd1ec2e487259bdd219745c9a0
Izabela <izabela.m.alfenas@gmail.com>
2022-02-22 17:34:12 -0300

    ft: get gender data

---
+++ src/useCases/GetGenderData.ts
@@ -0,0 +1,30 @@
+import { GetProgramData } from "./GetProgramData";
+
+interface IData {
+  title: string;
+  total: number;
+  percentage: number;
+  updatedAt: string;
+}
+
+export class GetGenderData {
+  constructor(private getProgramData: GetProgramData) { }
+
+  async run(): Promise<any> {
+    const { res } = await this.getProgramData.run();
+
+    const rawData = res.genders;
+    const data: Array<IData> = [];
+
+    rawData.map((gender) => {
+      data.push({
+        title: gender.description,
+        total: gender.total,
+        percentage: gender.totalRaw * 100,
+        updatedAt: res.updated_at
+      });
+    });
+
+    return data;
+  }
+}
